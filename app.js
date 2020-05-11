59e18a9c30c94243bfac786c752ceb48c0f5d226
Your Name <you@example.com>
2020-05-11 22:25:11 +0530

    Added Procfile and port

--- app.js
+++ app.js
@@ -328,9 +328,12 @@ app.post("/compose",function(req,res) {
     })
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port!");
 });
